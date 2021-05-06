1548e38d1d0c3804ae1fde223ef7114c321c2542
praewspc <praewspc@gmail.com>
2021-05-06 11:50:29 +0700

    praewupdate_060564

--- src/Components/BlockCreate.js
+++ src/Components/BlockCreate.js
@@ -3,17 +3,15 @@ import { Link, useHistory } from "react-router-dom";
 import { GlobalContext } from "../Context/GlobalState";
 import { Row, Col } from "react-bootstrap";
 // import { Form ,FormGroup } from 'react-bootstrap';
-import Container from "@material-ui/core/Container";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import RemoveIcon from "@material-ui/icons/Remove";
-import AddIcon from "@material-ui/icons/Add";
-import Icon from "@material-ui/core/Icon";
 import { makeStyles } from "@material-ui/core/styles";
 // import { Input } from '@material-ui/core';
 import { v4 as uuid } from "uuid";
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
 import axios from "axios";
+import Icon from '@material-ui/core/Icon';
+import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
 import {
   Form,
   FormGroup,
@@ -21,7 +19,6 @@ import {
   Input,
   // Button
 } from "reactstrap";
-import { set } from "mobx";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -31,8 +28,8 @@ const useStyles = makeStyles((theme) => ({
   },
   button: {
     margin: theme.spacing(1),
-    marginLeft: "48px",
-    marginTop: "50px"
+    marginLeft: "50px",
+    marginTop: "50px",
   },
   button2: {
     margin: theme.spacing(1),
@@ -42,46 +39,24 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export const BlockCreate = () => {
-
   const classes = useStyles();
-
-  const [name, setName] = useState("");
   const { addUser } = useContext(GlobalContext);
   const history = useHistory();
 
-  const [inputNameGroup, setInputGroupName] = useState([
-    { groupname: "" }]);
-  const [inputFields, setInputFields] = useState([
-    { firstName: "", lastName: "" },
-  ]);
-  const [inputFields2, setInputFields2] = useState([
-    { firstName: "", lastName: "" },
-  ]);
-
-  const onChange = (e) => {
-    // setName(e.target.value);
-    setGroupname(e.target.value)
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-  };
-
-  const onSubmit = () => {
-    const newUser = {
-      id: uuid(),
-      groupname
-    };
-    addUser(newUser);
-    history.push("/group");
-    console.log("InputFields NameGroup", groupname);
-    console.log("InputFields FirstName Member: ", memberfirstname);
-    console.log("InputFields LastName Member: ", memberlastname);
-    console.log("InputFields FirstName Advisor: ", advisorfirstname);
-    console.log("InputFields LastName Advisor: ", advisorlastname);
-  };
-
+  // const componentDidMount = () => {
+  //     axios.get('http://localhost:5000/group/')
+  //       .then(response => {
+  //         if (response.data.length > 0) {
+  //           this.setState({
+  //             // users: response.data.map(user => user.username),
+  //             // username: response.data[0].username
+  //           })
+  //         }
+  //       })
+  //       .catch((error) => {
+  //         console.log(error);
+  //       })}
+  let h = useHistory();
   const [groupname, setGroupname] = useState("");
   const [memberfirstname, setmemberFirstname] = useState("");
   const [memberlastname, setmemberLastname] = useState("");
@@ -89,6 +64,16 @@ export const BlockCreate = () => {
   const [advisorlastname, setadvisorlastname] = useState("");
 
   const [groupList, setgroupList] = useState([]);
+
+  const changeGroup = () => {
+    const newUser = {
+      id: uuid(),
+      groupname
+    }
+    addUser(newUser);
+    history.push('/group');
+  }
+
   const addGroup = () => {
     axios
       .post("http://localhost:5000/group/add", {
@@ -111,130 +96,92 @@ export const BlockCreate = () => {
         ]);
       });
   };
-  // console.log(groupname);
-
+  console.log(groupname);
 
   return (
     <div className="rightblockcreate">
       <div className="blockwhitecreate">
-      <h1 className="TopnameCreate">Create Group</h1>
-
-      <hr className="hr-create"></hr>
+        <h1 className="TopnameCreate">Create Group</h1>
+        <hr className="hr-create"></hr>
 
         <Container>
-          <Form onSubmit={onSubmit}>
+          <Form onSubmit={changeGroup}>
+
             <h4 className="GroupName">Group Name</h4>
-
-            <div className="fieldmember"  >
-              <TextField
-                name="groupname"
-                label="Enter Name"
-                variant="outlined"
-                type="text"
-                // value={name} 
-                onChange={onChange}
-                value={groupname.setGroupname}
-              // onChange={event => handleChangeInput0(index0, event)}
-              />
-            </div>
-
-
-
-            <h4 className="Add-Member">Member</h4>
-
-            <form className={classes.root} >
-            <div className="fieldmember"  >
-                  <TextField
-                    name="memberfirst"
-                    label="First Name"
-                    variant="outlined"
-                    value={memberfirstname.setmemberFirstname}//inputField.firstName
-                    // onChange={(event) => handleChangeInput(index, event)}
-                    // onChange={onChange}
-                    onChange={(e)=>{
-                      setmemberFirstname(e.target.value);
-                    }}
-                  />
-                  <TextField
-                    name="memberlast"
-                    label="Last Name"
-                    variant="outlined"
-                    value={memberlastname.setmemberLastname}
-                    // onChange={(event) => handleChangeInput(index, event)}
-                    // onChange={onChange}
-                    onChange={(e)=>{
-                      setmemberLastname(e.target.value);
-                    }}
-                  />
-                  <IconButton >
-                    <RemoveIcon />
-                  </IconButton>
-
-                  <IconButton >
-                    <AddIcon />
-                  </IconButton>
-                </div>
-            </form>
-
-            <h4 className="Add-Advisor">Advistor</h4>
-
-            <form className={classes.root} >
-              
-            <div className="fieldmember"  >
-                  <TextField
-                    name="advisorfirst"
-                    label="First Name"
-                    variant="outlined"
-                    value2={advisorfirstname.setadvisorFirstname}
-                    // onChange={(event) => handleChangeInput2(index2, event)}
-                    onChange={(e)=>{
-                      setadvisorFirstname(e.target.value);
-                    }}
-                  />
-                  <TextField
-                    name="advisorlast"
-                    label="Last Name"
-                    variant="outlined"
-                    value2={advisorlastname.setadvisorlastname}
-                    // onChange={(event) => handleChangeInput2(index2, event)}
-                    onChange={(e)=>{
-                      setadvisorlastname(e.target.value);
-                    }}
-                  />
-                  <IconButton >
-                    <RemoveIcon />
-                  </IconButton>
-
-                  <IconButton >
-                    <AddIcon />
-                  </IconButton>
-              
+            <form className={classes.root}>
+              <div className="fieldmember">
+                <TextField
+                  type="text"
+                  className="from-control"
+                  variant="outlined"
+                  label="Enter Group Name"
+                  onChange={(event) => {
+                    setGroupname(event.target.value);
+                  }}
+                />
               </div>
             </form>
 
-            <Button  onClick={addGroup}
-              id="btn-create"
+            <h4 className="Add-Member">Member</h4>
+            <form className={classes.root}>
+              <div className="fieldmember">
+                <TextField
+                  type="text"
+                  variant="outlined"
+                  className="from-control"
+                  label="Firstname Member"
+                  onChange={(event) => {
+                    setmemberFirstname(event.target.value);
+                  }}
+                />
+                <TextField
+                  type="text"
+                  variant="outlined"
+                  className="from-control"
+                  label="Lastname Member"
+                  onChange={(event) => {
+                    setmemberLastname(event.target.value);
+                  }}
+                />
+              </div>
+            </form>
+            
+            <h4 className="Add-Advisor">Advistor</h4>
+            <form className={classes.root}>
+              <div className="fieldmember">
+                <TextField
+                  type="text"
+                  variant="outlined"
+                  className="from-control"
+                  label="Firstname Advisor"
+                  onChange={(event) => {
+                    setadvisorFirstname(event.target.value);
+                  }}
+                />
+                <TextField
+                  type="text"
+                  className="from-control"
+                  variant="outlined"
+                  label="Lastname Advisor"
+                  onChange={(event) => {
+                    setadvisorlastname(event.target.value);
+                  }}
+                />
+              </div>
+            </form>
+            
+
+
+            <Button
               className={classes.button}
-              variant="outlined"
+              onClick={addGroup}
               color="primary"
               type="submit"
-              endIcon={<Icon>add</Icon>}
+              variant="outlined"
+              endIcon={<Icon>close</Icon>}
             >
               Submit
             </Button>
-
-            <Button
-              id="btn-cancel"
-              className={classes.button2}
-              variant="outlined"
-              color="primary"
-              type="submit"
-              endIcon={<Icon>close</Icon>}
-              onClick={handleSubmit}
-            >
-              <Link to="/project">Cancel</Link>
-            </Button>
-            {/* <Link to="/project" className="btn btn-danger ml-2">Cancel</Link> */}
           </Form>
         </Container>
       </div>
