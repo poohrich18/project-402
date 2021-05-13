3dcbb174d97065fba183f41105f167a3241a844e
Phurit <poohrich1818@hotmail.com>
2021-05-14 04:13:41 +0700

    abcd

--- src/Components/Blockproject.js
+++ src/Components/Blockproject.js
@@ -6,21 +6,21 @@ import Icon from "@material-ui/core/Icon";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import { ProjectList } from "./ProjectList";
-import BorderAllIcon from '@material-ui/icons/BorderAll';
+import BorderAllIcon from "@material-ui/icons/BorderAll";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
   button2: {
     margin: theme.spacing(1),
     marginLeft: "24px",
-    marginTop: "20px"
+    marginTop: "20px",
   },
   button3: {
     margin: theme.spacing(1),
     marginLeft: "4px",
-    marginTop: "20px"
+    marginTop: "20px",
   },
 }));
 
@@ -29,15 +29,77 @@ export const Blockproject = () => {
 
   const [groupList, setgroupList] = useState([]);
 
+  const [newgroupname, setnewgroupname] = useState([]);
+  const [newfirstname, setnewfirstname] = useState([]);
+  const [newlastname, setnewlastname] = useState([]);
+  const [newadvisorfirstname, setnewadvisorfirstname] = useState([]);
+  const [newadvisorlastname, setnewadvisorlastname] = useState([]);
+
   const getGroup = () => {
     axios.get("http://localhost:5000/group/").then((respond) => {
       setgroupList(respond.data);
     });
   };
 
+  useEffect(() => {
+    getGroup();
+  }, []);
+
+  const updateGroupname = (id) => {
+    axios.put("http://localhost:5000/group/update1/", {
+      groupname: newgroupname,
+      id: id,
+    });
+  };
+  const updateMemberFirstname = (id) => {
+    axios.put("http://localhost:5000/group/update2/", {
+      memberfirstname: newfirstname,
+      id: id,
+    });
+  };
+  const updateMemberLastname = (id) => {
+    axios.put("http://localhost:5000/group/update3/", {
+      memberlastname: newlastname,
+      id: id,
+    });
+  };
+  const updateAvisorFirstname = (id) => {
+    axios.put("http://localhost:5000/group/update4/", {
+      advisorfirstname: newadvisorfirstname,
+      id: id,
+    });
+  };
+  const updateAvisorLastname = (id) => {
+    axios.put("http://localhost:5000/group/update5/", {
+      advisorlaststname: newadvisorlastname,
+      id: id,
+    });
+  };
+  // const updateGroupname = (id) => {
+  //   axios
+  //     .put("http://localhost:5000/group/update1/", {
+  //       groupname: newgroupname,
+  //       id: id,
+  //     })
+  //     .then((respond) => {
+  //       setgroupList(
+  //         groupList.map((val) => {
+  //           return val.id == id ? {
+  //             id: val.id,
+  //             groupname: newgroupname,
+  //             memberfirstname:val.memberfirstname,
+  //             memberlastname:val.memberlastname,
+  //             advisorfirstname: val.advisorfirstname
+
+  //           }:val;
+
+  //         })
+  //       );
+  //     });
+  // };
+
   return (
     <div className="rightblockproject">
-
       <div className="blockwhiteproject">
         <h1 className="TopnameGroup">Group</h1>
         <hr className="hr-group"></hr>
@@ -50,16 +112,18 @@ export const Blockproject = () => {
             type="submit"
             endIcon={<Icon>add</Icon>}
           >
-          <Link to="/create">Add Group</Link>
+            <Link to="/create">Add Group</Link>
           </Button>
 
-          <Button 
-            className={classes.button3} 
+          <Button
+            className={classes.button3}
             variant="outlined"
             color="primary"
             type="submit"
             endIcon={<BorderAllIcon />}
-            onClick={getGroup}> Show All Group
+          >
+            {" "}
+            Show All Group
           </Button>
           {/* <br></br>
           <br></br> */}
@@ -67,13 +131,94 @@ export const Blockproject = () => {
             return (
               <div className=" group list">
                 <div className="card-body text-left">
-                  <p className="card-text">Groupname: {val.groupname}</p>
-                  <p className="card-text">Firstname Member: {val.memberfirstname}</p>
-                  <p className="card-text">Lastname Member: {val.memberlastname}</p>
-                  <p className="card-text">Adivisor: {val.advisorfirstname}</p>
+                  <p className="card-text">
+                    Groupname: {val.groupname}
+                    </p>
+                  <p className="card-text">
+                    Firstname Member: {val.memberfirstname}
+                  </p>
+                  <p className="card-text">
+                    Lastname Member: {val.memberlastname}
+                  </p>
+                  <p className="card-text">
+                    Firstname Adivisor : {val.advisorfirstname}
+                  </p>
+                  <p className="card-text">
+                    Lastname Adivisor : {val.advisorlaststname}
+                  </p>
+                  <div className="d-flex">
+                    <input
+                      type="text"
+                      type="String"
+                      placeholder="Gropname"
+                      onChange={(e) => {
+                        setnewgroupname(e.target.value);
+                      }}
+                    />
+                    <button
+                      className="btn btn-warning"
+                      onClick={() => {
+                        updateGroupname(val._id);
+                      }}
+                    >
+                      Update
+                    </button>
+                  </div>
+                  <div className="d-flex">
+                    <input
+                      type="text"
+                      type="String"
+                      placeholder="Firstname"
+                      onChange={(e) => {
+                        setnewfirstname(e.target.value);
+                      }}
+                    ></input>
+                    <button className="btn btn-warning"onClick={() => {
+                        updateMemberFirstname(val._id);
+                      }}>Update</button>
+                  </div>
+                  <div className="d-flex">
+                    <input
+                      type="text"
+                      type="String"
+                      placeholder="Lastname"
+                      onChange={(e) => {
+                        setnewlastname(e.target.value);
+                      }}
+                    ></input>
+                    <button className="btn btn-warning"onClick={() => {
+                        updateMemberLastname(val._id);
+                      }}>Update</button>
+                  </div>
+                  <div className="d-flex">
+                    <input
+                      type="text"
+                      type="String"
+                      placeholder="Adivisor Firstname"
+                      onChange={(e) => {
+                        setnewadvisorfirstname(e.target.value);
+                      }}
+                    ></input>
+                    <button className="btn btn-warning"onClick={() => {
+                        updateAvisorFirstname(val._id);
+                      }}>Update</button>
+                  </div>
+                  <div className="d-flex">
+                    <input
+                      type="text"
+                      type="String"
+                      placeholder="Adivisor Lastname"
+                      onChange={(e) => {
+                        setnewadvisorlastname(e.target.value);
+                      }}
+                    ></input>
+                    <button className="btn btn-warning"onClick={() => {
+                        updateAvisorLastname(val._id);
+                      }}>Update</button>
+                  </div>
                 </div>
               </div>
-            )
+            );
           })}
         </Container>
 
